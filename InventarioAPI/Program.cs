6e276903bb5b2d8ac5edbcbf5163cb5eed33405b
AntonioMU21 <antonioamores5744@gmail.com>
2026-02-17 13:01:13 +0100

    Ahora es mas legible

--- InventarioAPI/Program.cs
+++ InventarioAPI/Program.cs
@@ -3,13 +3,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// --- CONFIGURACIÓN DE LA BASE DE DATOS ---
-// Ahora lee la configuración del archivo appsettings.json de forma profesional.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("CadenaConexion")));
 
-// --- CONFIGURACIÓN DE CORS ---
-// Permite que Angular se comunique con esta API
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirAngular", policy =>
@@ -26,14 +22,12 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configuración para el entorno de desarrollo (Swagger)
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-// Activamos la política de CORS definida arriba
 app.UseCors("PermitirAngular");
 
 app.MapControllers();
